d131a8afdd573c6cc6caa6449db46eac8aa41130
DanalaBhavya <dhanalabhavya1555@gmail.com>
2024-07-19 15:30:29 +0530

    commit

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ import streamlit as st
 import os
 from PIL import Image
 import google.generativeai as genai
-
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-model=genai.GenerativeModel('gemini-pro-vision')
-
+model=genai.GenerativeModel('gemini-1.5-flash')
 def get_gemini_response(input,image,prompt):
     response=model.generate_content([input,image[0],prompt])
     return response.text
@@ -24,7 +22,6 @@ def input_image_details(uploaded_file):
         return image_parts
     else:
        raise FileNotFoundError("No File Uploaded") 
-   
 st.set_page_config(page_title="MultiLanguage Invoice Extractor")
 st.header("MultiLanguage Invoice Extractor")
 input=st.text_input("Input Prompt: ",key="input")
@@ -39,13 +36,11 @@ input_prompt="""
 You are an expert in understanding invoices.We will upload image as invoice and you will have to
 answer any questions based on the uploaded invoice image
 """    
-
 if submit:
     image_data=input_image_details(uploaded_file)
     response=get_gemini_response(input_prompt,image_data,input)
     st.subheader("The Response is")
     st.write(response)
-    
     
     
     
